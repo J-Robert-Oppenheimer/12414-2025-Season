b60940e10c54c9fe3eb2fbdaaa056a6346caff31
MartiniMaster <alexander.lewis.martin0530@gmail.com>
2025-01-24 13:15:52 -0500

    it's time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PedroPascal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PedroPascal.java
@@ -35,7 +35,7 @@ public class PedroPascal extends OpMode {
     private int pathState;
 
 
-    private PathChain obs2Spec5, spec2Park;
+    private PathChain obs2Spec5, spec52SubIntake, subIntake2SubDrop, subDrop2MidIntake, midIntake2MidDrop, midDrop2WallIntake, wallIntake2WallDrop, wallDrop2Park;
 
     public void buildPaths() {
         Constants.setConstants(FConstants.class, LConstants.class);
@@ -45,27 +45,98 @@ public class PedroPascal extends OpMode {
                 .setLinearHeadingInterpolation(SP.ObsZoneStart.getHeading(), SP.Specimen5.getHeading())
                 .build();
 
-        spec2Park = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(SP.Specimen5), new Point(SP.ObsPark)))
-                .setLinearHeadingInterpolation(SP.Specimen5.getHeading(), SP.ObsPark.getHeading())
+        spec52SubIntake = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(SP.Specimen5), new Point(SP.SampleIntakeObsSub)))
+                .setLinearHeadingInterpolation(SP.Specimen5.getHeading(), SP.SampleIntakeObsSub.getHeading())
+                .build();
 
+        subIntake2SubDrop = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(SP.SampleIntakeObsSub), new Point(SP.SampleDropSub)))
+                .setLinearHeadingInterpolation(SP.SampleIntakeObsSub.getHeading(), SP.SampleDropSub.getHeading())
+                .build();
+
+        subDrop2MidIntake = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(SP.SampleDropSub), new Point(SP.SampleIntakeObsMid)))
+                .setLinearHeadingInterpolation(SP.SampleDropSub.getHeading(), SP.SampleIntakeObsMid.getHeading())
+                .build();
+
+        midIntake2MidDrop = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(SP.SampleIntakeObsMid), new Point(SP.SampleDropMid)))
+                .setLinearHeadingInterpolation(SP.SampleIntakeObsMid.getHeading(), SP.SampleDropMid.getHeading())
+                .build();
+
+        midDrop2WallIntake = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(SP.SampleDropMid), new Point(SP.SampleIntakeObsWall)))
+                .setLinearHeadingInterpolation(SP.SampleDropMid.getHeading(), SP.SampleIntakeObsWall.getHeading())
+                .build();
+
+        wallIntake2WallDrop = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(SP.SampleIntakeObsWall), new Point(SP.SampleDropWall)))
+                .setLinearHeadingInterpolation(SP.SampleIntakeObsWall.getHeading(), SP.SampleDropWall.getHeading())
+                .build();
+
+        wallDrop2Park = follower.pathBuilder()
+                .addPath(new BezierCurve(new Point(SP.SampleDropWall), new Point(65, 137), new Point(SP.ObsPark)))
+                .setLinearHeadingInterpolation(SP.SampleDropWall.getHeading(), SP.ObsPark.getHeading())
                 .build();
     }
 
     public void autonomousPathUpdate() {
         switch (pathState) {
             case 0: // Move from start to scoring position
-                follower.followPath(obs2Spec5);
-
-
-                setPathState(1);
-
+                if (!follower.isBusy()) {
+                    follower.followPath(obs2Spec5);
+                    setPathState(1);
+                }
                 break;
 
             case 1:
                 if (!follower.isBusy()){
-                    follower.followPath(spec2Park)
+                    follower.followPath(spec52SubIntake);
+                    setPathState(2);
                 }
+                break;
+
+            case 2:
+                if (!follower.isBusy()){
+                    follower.followPath(subIntake2SubDrop);
+                    setPathState(3);
+                }
+                break;
+
+            case 3:
+                if (!follower.isBusy()){
+                    follower.followPath(subDrop2MidIntake);
+                    setPathState(4);
+                }
+                break;
+
+            case 4:
+                if (!follower.isBusy()){
+                    follower.followPath(midIntake2MidDrop);
+                    setPathState(5);
+                }
+                break;
+
+            case 5:
+                if (!follower.isBusy()){
+                    follower.followPath(midDrop2WallIntake);
+                    setPathState(6);
+                }
+                break;
+
+            case 6:
+                if (!follower.isBusy()){
+                    follower.followPath(wallIntake2WallDrop);
+                    setPathState(7);
+                }
+                break;
+
+            case 7:
+                if (!follower.isBusy()){
+                    follower.followPath(wallDrop2Park);
+                }
+                break;
         }
     }
 
